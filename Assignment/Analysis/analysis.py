da4bc1a89d88d496f9a48863416440f63e43a1f1
Manoj Kesavulu <manojpratnas@gmail.com>
2018-12-04 21:47:40 +0000

    initial libraries

---
+++ Assignment/Analysis/analysis.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from sklearn import datasets, linear_model
+from sklearn.model_selection import train_test_split
+import sklearn.metrics as skm
+from matplotlib import pyplot as plt
+
